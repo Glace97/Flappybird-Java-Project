3934918a895da6443b6aade3b7c35f4ad7db1cd2
Glace97 <glacier.ali@gmail.com>
2020-04-21 21:33:43 +0200

    first commit

---
+++ Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglGraphics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Flappybird(), config); //open window
+
+	}
+}
