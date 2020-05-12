abb666da8d95eee2753ef622e9f2a7a0c1d7497c
Glace97 <glacier.ali@gmail.com>
2020-05-12 15:46:53 +0200

    extending simple game

--- Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglGraphics;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Flappybird(), config); //open window
+		new LwjglApplication(, config); //open window
 
 	}
 }
---
+++ Game/desktop/src/com/mygdx/game/desktop/Flappy.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+//Game är en abstract klass, kan inte intantieras
+public class Flappy extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    public void create() {
+        batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        //setScreen metoden sätter nytt obj av MainmenuScreenm spelinstance, flappybird
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render(); //important! . Without this call, the Screen that you set in the create() method will not be rendered!
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+
+}
