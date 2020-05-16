057c5ef11f2300a505953c5418b60069581ed8a2
Glace97 <glacier.ali@gmail.com>
2020-05-16 21:46:14 +0200

    extended game fixed

--- Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglGraphics;
@@ -7,7 +8,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglGraphics;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(, config); //open window
+		config.title = "Flappy";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Flappy(), config);
 
 	}
+
 }
